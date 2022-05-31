c6ad0c89a343fce87bb81ae0d6986f4c918ae3f8
GitHub Action <action@github.com>
2022-05-31 09:16:57 +0000

    Template cleanup

--- src/main/kotlin/com/github/arzefromchina/tempate/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/arzefromchina/tempate/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.arzefromchina.tempate.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.arzefromchina.tempate.MyBundle
 
 class MyApplicationService {
 
